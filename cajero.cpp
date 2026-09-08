3c9cf10943bf3cd7c61afa22dc027fc909625600
“DanRosales” <00155426@uca.edu.sv>
2026-09-08 14:35:48 -0600

    feature:added menu options

---
+++ cajero.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+bool salir=0;
+
+int main (){
+    int operando;
+    int opcion;
+    while(salir==0){
+        cout<<"Elija una opcion a realizar:\n";
+        cout<<"1. Consultar \n"<<
+              "2. Depositar \n"<<
+              "3. Retirar \n"<<
+              "4. Salir \n\n";
+        cin>>opcion;
+        switch(opcion){
+            case 1:
+                
+            break;
+
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+            salir=1;
+            cout<<"Gracias por usar nuestro sistema\n";
+            break;
+
+            default:
+            cout<<"Error de opcion\n";
+            return 1;
+            break;
+        }
+    }
+
+    return 0;
+}
